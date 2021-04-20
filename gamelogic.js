34ceb547cb12ed1ca097e6f889edec9142941fef
RianneH1990 <74492460+RianneH1990@users.noreply.github.com>
2021-04-20 21:14:54 +0200

    Display words so far test 1 passed

--- gamelogic.js
+++ gamelogic.js
@@ -1,5 +1,32 @@
+// Input:
+// word: javascript
+// guesses: ["a", "t", "i", "b", "k"]
+
+// Output:
+// _ a _ a _ _ _ i _ t
+
+// Input:
+// word: javascript
+// guesses: []
+
+// Output:
+// _ _ _ _ _ _ _ _ _ _
+
+// Steps:
+// - Transformeren
+// - For loop maken:
+//    - Elke letter vervangen door een "_ "
+//    - Tot we alle letters gehad hebben
+// - return resultaat
+
+
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  console.log("word:", word, "Guesses:", guesses);
+  let output = "";
+  for (let i = 0; i < word.length; i++){
+    output = output + "_ ";
+  }
+  return output;
 }
 
 function isGameWon(word, guesses) {
