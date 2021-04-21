08ac3f58624bb270baebfbb2aa7069a54e9b1075
RianneH1990 <74492460+RianneH1990@users.noreply.github.com>
2021-04-21 10:32:24 +0200

    display word so far test 1 changed to a if statement

--- gamelogic.js
+++ gamelogic.js
@@ -5,27 +5,33 @@
 // Output:
 // _ a _ a _ _ _ i _ t
 
-// Input:
-// word: javascript
-// guesses: []
+// DOEL:
+// WOORD: javascript, GUESSES: [] -> "_ _ _ _ _ _ _ _ _ _ "
+// WOORD: javascript, GUESSES: ['a', 'b'] -> "_ a _ a _ _ _ _ _ _ "
 
-// Output:
-// _ _ _ _ _ _ _ _ _ _
-
-// Steps:
-// - Transformeren
-// - For loop maken:
-//    - Elke letter vervangen door een "_ "
-//    - Tot we alle letters gehad hebben
-// - return resultaat
-
+// - [x] javascript "splitten" split() -> ['j','a','v','a','s','c','r','i','p','t']
+// - [x] een loop maken -> ['j','a','v','a','s','c','r','i','p','t']
+// - [x] checken: zit de letter in guesses?
+// - [ ] if else
+// - [x] zo nee: "_ "
+// - [ ] zo ja: "j " (niet j, maar de letter die we dan checken)
+// - [ ] klaar? -> onze string die we hebben gemaakt returnen
 
 function displayWordSoFar(word, guesses) {
-  console.log("word:", word, "Guesses:", guesses);
+  //console.log("word:", word, "Guesses:", guesses);
+  const wordSplit = word.split("");
+  //console.log(wordSplit);
   let output = "";
-  for (let i = 0; i < word.length; i++){
-    output = output + "_ ";
-  }
+  for (let i = 0; i < wordSplit.length; i++){
+    const letterInWord = wordSplit[i];
+    //console.log("1 LETTER OF WORD TO GUESS:", letterInWord);
+    const letterGuessed = guesses.includes(letterInWord);
+    //console.log("GERADEN LETTERS: ", letterGuessed);
+    if (!letterGuessed) {
+      output = output + "_ ";
+      //console.log(output);
+    }
+    }
   return output;
 }
 
